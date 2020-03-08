aace317f850fcc6388080041930ca843fc02f0d8
John Watson <johnwatson484@hotmail.com>
2020-03-08 17:12:14 +0000

    Column names updated

---
+++ PowerLinesAnalysisService/Migrations/20200308171056_Initial.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace PowerLinesAnalysisService.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "results",
+                columns: table => new
+                {
+                    resultId = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    division = table.Column<string>(nullable: true),
+                    date = table.Column<DateTime>(nullable: false),
+                    homeTeam = table.Column<string>(nullable: true),
+                    awayTeam = table.Column<string>(nullable: true),
+                    fullTimeHomeGoals = table.Column<int>(nullable: false),
+                    fullTimeAwayGoals = table.Column<int>(nullable: false),
+                    fullTimeResult = table.Column<string>(nullable: true),
+                    halfTimeHomeGoals = table.Column<int>(nullable: false),
+                    halfTimeAwayGoals = table.Column<int>(nullable: false),
+                    halfTimeResult = table.Column<string>(nullable: true),
+                    homeOddsAverage = table.Column<decimal>(nullable: false),
+                    drawOddsAverage = table.Column<decimal>(nullable: false),
+                    awayOddsAverage = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_results", x => x.resultId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "results");
+        }
+    }
+}
--- PowerLinesAnalysisService/Models/Result.cs
+++ PowerLinesAnalysisService/Models/Result.cs
@@ -6,32 +6,46 @@ namespace PowerLinesAnalysisService.Models
     [Table("results")]
     public class Result
     {
+        [Column("resultId")]
         public int ResultId { get; set; }
 
+        [Column("division")]
         public string Division { get; set; }
 
+        [Column("date")]
         public DateTime Date { get; set; }
 
+        [Column("homeTeam")]
         public string HomeTeam { get; set; }
 
+        [Column("awayTeam")]
         public string AwayTeam { get; set; }
 
+        [Column("fullTimeHomeGoals")]
         public int FullTimeHomeGoals { get; set; }
 
+        [Column("fullTimeAwayGoals")]
         public int FullTimeAwayGoals { get; set; }
 
+        [Column("fullTimeResult")]
         public string FullTimeResult { get; set; }
 
+        [Column("halfTimeHomeGoals")]
         public int HalfTimeHomeGoals { get; set; }
 
+        [Column("halfTimeAwayGoals")]
         public int HalfTimeAwayGoals { get; set; }
 
+        [Column("halfTimeResult")]
         public string HalfTimeResult { get; set; }
 
+        [Column("homeOddsAverage")]
         public decimal HomeOddsAverage { get; set; }
 
+        [Column("drawOddsAverage")]
         public decimal DrawOddsAverage { get; set; }
 
+        [Column("awayOddsAverage")]
         public decimal AwayOddsAverage { get; set; }
     }
 }
