75189aca05e3f2a033bd25a03af0c838401a9c04
John Watson <johnwatson484@hotmail.com>
2020-03-14 18:17:22 +0000

    Calculation flow added

--- PowerLinesAnalysisService/Analysis/AnalysisService.cs
+++ PowerLinesAnalysisService/Analysis/AnalysisService.cs
@@ -7,6 +7,43 @@ namespace PowerLinesAnalysisService.Analysis
     {
         public MatchOdds GetMatchOdds(Fixture fixture)
         {
+            // calc avg home goals
+
+            // calc avg away goals
+
+            // calc avg goals for home team
+
+            // calc avg goals for away team
+
+            // calc avg conceded by home team
+
+            // calc avg conceded by away team
+
+            // Number of goals scored at home last season by the home team / number of home games played
+
+            // Home team's average goals per home game / average home league goals per game
+
+            // Number of goals conceded away last season by the away team / number of away games played
+
+            // Away team's average goals conceded per away game / average away league goals conceded per game
+
+            // Home team attack strength * away team defence strength * average number of home goals
+
+            // Number of goals scored away last season by the away team / number of away games played
+
+            // Away team's average goals per away game / average away league goals per game
+
+            // Home team's average goals conceded per home game / number of home games played
+
+            // Home team's average goals conceded per home game / average home league goals per game
+
+            // Away team attack strength * home team defence strength * average number of away goals
+
+            // poisson for all goals for both teams
+
+            // calc 1x2
+
+            // calc likely score
             return new MatchOdds();
         }
     }
