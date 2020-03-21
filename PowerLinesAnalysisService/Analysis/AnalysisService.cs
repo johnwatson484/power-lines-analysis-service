89bd080be011e683bc2d6e8333e2d01aab01a0b9
John Watson <johnwatson484@hotmail.com>
2020-03-21 21:54:55 +0000

    AnalysisService refactored

--- PowerLinesAnalysisService/Analysis/AnalysisService.cs
+++ PowerLinesAnalysisService/Analysis/AnalysisService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using PowerLinesAnalysisService.Data;
 using PowerLinesAnalysisService.Models;
+using PowerLinesAnalysisService.Extensions;
 
 namespace PowerLinesAnalysisService.Analysis
 {
@@ -13,6 +14,8 @@ namespace PowerLinesAnalysisService.Analysis
         const int maxGoalsPerGame = 5;
         DateTime startDate;
         List<Result> matches;
+        decimal homeExpectedGoals;
+        decimal awayExpectedGoals;
         GoalDistribution goalDistribution;
         Poisson poisson;
         OddsCalculator oddsService;
@@ -29,32 +32,8 @@ namespace PowerLinesAnalysisService.Analysis
             SetStartDate(fixture.Date);
             SetAnalysisMatches(fixture.Division);
 
-            var totalAverageHomeGoals = GetTotalAverageHomeGoals();
-            var totalAverageAwayGoals = GetTotalAverageAwayGoals();
-            var totalAverageHomeConceded = totalAverageAwayGoals;
-            var totalAverageAwayConceded = totalAverageHomeGoals;
-
-            var averageHomeGoals = GetAverageHomeGoals(fixture.HomeTeam);
-            var homeAttackStrength = GetAttackStrength(averageHomeGoals, totalAverageHomeGoals);
-            var averageAwayConceded = GetAverageAwayConceded(fixture.AwayTeam);
-            var awayDefenceStrength = GetDefenceStrength(averageAwayConceded, totalAverageAwayConceded);
-            var homeExpectedGoals = GetExpectedGoals(homeAttackStrength, awayDefenceStrength, totalAverageHomeGoals);
-
-            var averageAwayGoals = GetAverageAwayGoals(fixture.AwayTeam);
-            var awayAttackStrength = GetAttackStrength(averageAwayGoals, totalAverageAwayGoals);
-            var averageHomeConceded = GetAverageHomeConceded(fixture.HomeTeam);
-            var homeDefenceStrength = GetDefenceStrength(averageHomeConceded, totalAverageHomeConceded);
-            var awayExpectedGoals = GetExpectedGoals(awayAttackStrength, homeDefenceStrength, totalAverageAwayGoals);
-
-            for(int goals = 0; goals <= maxGoalsPerGame; goals++)
-            {
-                var homeGoalProbability = new GoalProbability(goals, (decimal)poisson.GetProbability(goals, (double)homeExpectedGoals));
-                goalDistribution.HomeGoalProbabilities.Add(homeGoalProbability);
-                var awayGoalProbability = new GoalProbability(goals, (decimal)poisson.GetProbability(goals, (double)awayExpectedGoals));
-                goalDistribution.AwayGoalProbabilities.Add(awayGoalProbability);
-            }
-
-            goalDistribution.CalculateDistribution();
+            CalculateExpectedGoals(fixture);
+            CalculateGoalDistribution();
 
             oddsService = new OddsCalculator(fixture.FixtureId, goalDistribution);
             return oddsService.GetMatchOdds();
@@ -71,53 +50,89 @@ namespace PowerLinesAnalysisService.Analysis
             matches = dbContext.Results.Where(x => x.Division == division && x.Date >= startDate).ToList();
         }
 
+        private void CalculateExpectedGoals(Fixture fixture)
+        {
+            var totalAverageHomeGoals = GetTotalAverageHomeGoals();
+            var totalAverageAwayGoals = GetTotalAverageAwayGoals();
+            var totalAverageHomeConceded = totalAverageAwayGoals;
+            var totalAverageAwayConceded = totalAverageHomeGoals;
+
+            var averageHomeGoals = GetAverageHomeGoals(fixture.HomeTeam);
+            var homeAttackStrength = GetAttackStrength(averageHomeGoals, totalAverageHomeGoals);
+            var averageAwayConceded = GetAverageAwayConceded(fixture.AwayTeam);
+            var awayDefenceStrength = GetDefenceStrength(averageAwayConceded, totalAverageAwayConceded);
+            homeExpectedGoals = GetExpectedGoals(homeAttackStrength, awayDefenceStrength, totalAverageHomeGoals);
+
+            var averageAwayGoals = GetAverageAwayGoals(fixture.AwayTeam);
+            var awayAttackStrength = GetAttackStrength(averageAwayGoals, totalAverageAwayGoals);
+            var averageHomeConceded = GetAverageHomeConceded(fixture.HomeTeam);
+            var homeDefenceStrength = GetDefenceStrength(averageHomeConceded, totalAverageHomeConceded);
+            awayExpectedGoals = GetExpectedGoals(awayAttackStrength, homeDefenceStrength, totalAverageAwayGoals);
+        }
+
         private decimal GetTotalAverageHomeGoals()
         {
-            return decimal.Divide(matches.Sum(x => x.FullTimeHomeGoals), matches.Count);
+            return DecimalExtensions.SafeDivide(matches.Sum(x => x.FullTimeHomeGoals), matches.Count);
         }
 
         private decimal GetTotalAverageAwayGoals()
         {
-            return decimal.Divide(matches.Sum(x => x.FullTimeAwayGoals), matches.Count);
+            return DecimalExtensions.SafeDivide(matches.Sum(x => x.FullTimeAwayGoals), matches.Count);
         }
 
         private decimal GetAverageHomeGoals(string homeTeam)
         {
             var homeMatches = matches.Where(x => x.HomeTeam == homeTeam).ToList();
-            return decimal.Divide(homeMatches.Sum(x => x.FullTimeHomeGoals), homeMatches.Count);
+            return DecimalExtensions.SafeDivide(homeMatches.Sum(x => x.FullTimeHomeGoals), homeMatches.Count);
         }
 
         private decimal GetAverageAwayGoals(string awayTeam)
         {
             var awayMatches = matches.Where(x => x.AwayTeam == awayTeam).ToList();
-            return decimal.Divide(awayMatches.Sum(x => x.FullTimeAwayGoals), awayMatches.Count);
+            return DecimalExtensions.SafeDivide(awayMatches.Sum(x => x.FullTimeAwayGoals), awayMatches.Count);
         }
         
         private decimal GetAverageHomeConceded(string homeTeam)
         {
             var homeMatches = matches.Where(x => x.HomeTeam == homeTeam).ToList();
-            return decimal.Divide(homeMatches.Sum(x => x.FullTimeAwayGoals), homeMatches.Count);
+            return DecimalExtensions.SafeDivide(homeMatches.Sum(x => x.FullTimeAwayGoals), homeMatches.Count);
         }
 
         private decimal GetAverageAwayConceded(string awayTeam)
         {
             var awayMatches = matches.Where(x => x.AwayTeam == awayTeam).ToList();
-            return decimal.Divide(awayMatches.Sum(x => x.FullTimeHomeGoals), awayMatches.Count);
+            return DecimalExtensions.SafeDivide(awayMatches.Sum(x => x.FullTimeHomeGoals), awayMatches.Count);
         }
 
         private decimal GetAttackStrength(decimal averageGoals, decimal totalAverageGoals)
         {
-            return decimal.Divide(averageGoals, totalAverageGoals);
+            return DecimalExtensions.SafeDivide(averageGoals, totalAverageGoals);
         }
 
         private decimal GetDefenceStrength(decimal averageConceded, decimal totalAverageConceded)
         {
-            return decimal.Divide(averageConceded, totalAverageConceded);
+            return DecimalExtensions.SafeDivide(averageConceded, totalAverageConceded);
         }
 
         private decimal GetExpectedGoals(decimal teamAttackStrength, decimal oppositionDefenceStrength, decimal totalAverageGoals)
         {
             return teamAttackStrength * oppositionDefenceStrength * totalAverageGoals;
+        }
+
+        private void CalculateGoalDistribution()
+        {
+            for(int goals = 0; goals <= maxGoalsPerGame; goals++)
+            {
+                goalDistribution.HomeGoalProbabilities.Add(GetGoalProbability(goals, homeExpectedGoals));
+                goalDistribution.AwayGoalProbabilities.Add(GetGoalProbability(goals, awayExpectedGoals));
+            }
+
+            goalDistribution.CalculateDistribution();
+        }
+
+        private GoalProbability GetGoalProbability(int goals, decimal expectedGoals)
+        {
+            return new GoalProbability(goals, (decimal)poisson.GetProbability(goals, (double)expectedGoals));
         }
     }
 }
