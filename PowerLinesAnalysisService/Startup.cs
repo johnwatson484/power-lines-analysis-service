d2b4b087a27d11e723474fbdfad9960217115cee
John Watson <johnwatson484@hotmail.com>
2020-05-08 18:21:23 +0100

    Https redirect removed

--- PowerLinesAnalysisService/Startup.cs
+++ PowerLinesAnalysisService/Startup.cs
@@ -48,7 +48,6 @@ namespace PowerLinesAnalysisService
             {
                 app.UseDeveloperExceptionPage();
             }
-            app.UseHttpsRedirection();
             app.UseRouting();
             app.UseAuthorization();
             app.UseEndpoints(endpoints =>
